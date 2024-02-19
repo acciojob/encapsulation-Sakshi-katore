2df310d69ef776edd801db635427bdc7312272e6
Sakshi Katore <katoresakshi@gmail.com>
2024-02-19 19:19:34 +0530

    rw

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly rw = new RWOnly();
+    // Set value to name using setter function
+    rw.setName("John");
+    // Access value of name using getter function and print
+    System.out.println(rw.getName()); // Output: John
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    // Getter method for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter method for name
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
